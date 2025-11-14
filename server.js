ef27122f6fbc2d956ab7f9555fdd8cec7b4bb1b3
luksha14 <116290610+luksha14@users.noreply.github.com>
2025-11-14 01:30:49 +0100

    Refactor imports to use ES6 syntax
    Updated import statements to ES6 syntax and adjusted chatRoutes import.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const chatRoutes = require("./routes/chatRoutes");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import chatRoutes from "./routes/chatRoutes.js";
 
 dotenv.config();
 
